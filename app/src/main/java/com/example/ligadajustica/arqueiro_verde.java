16111469353145535ee19b12f9db03917c45429e
AugustoCarvalho <gutinho102004@gmail.com>
2020-07-20 23:48:58 -0300

    Initial commit

---
+++ app/src/main/java/com/example/ligadajustica/arqueiro_verde.java
@@ -0,0 +1,14 @@
+package com.example.ligadajustica;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class arqueiro_verde extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_arqueiro_verde);
+    }
+}
